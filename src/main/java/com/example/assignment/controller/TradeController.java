79f2deb00532f8f3d3044bc81850852f3723a748
ChunHisangLee <goldrushkl168@gmail.com>
2023-09-12 22:27:28 +0800

    2023/09/12 home -4 comply with SOLID principle

--- src/main/java/com/example/assignment/controller/TradeController.java
+++ src/main/java/com/example/assignment/controller/TradeController.java
@@ -1,10 +1,12 @@
 package com.example.assignment.controller;
 
-import com.example.assignment.entity.*;
+import com.example.assignment.entity.Account;
+import com.example.assignment.entity.Coin;
+import com.example.assignment.entity.History;
+import com.example.assignment.entity.TradeDirection;
 import com.example.assignment.service.*;
 import com.example.assignment.service.exception.ServiceException;
 import com.example.assignment.service.exception.UserNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,58 +19,84 @@ import java.util.Date;
 @RestController
 @RequestMapping("trade")
 public class TradeController {
-    @Autowired
-    private IUserService userService;
-    @Autowired
-    private IAccountService accountService;
-    @Autowired
-    private ICoinService coinService;
-    @Autowired
-    private IPriceService priceService;
-    @Autowired
-    private IHistoryService historyService;
+    private final IUserService userService;
+    private final IAccountService accountService;
+    private final ICoinService coinService;
+    private final IPriceService priceService;
+    private final IHistoryService historyService;
+
+    public TradeController(IUserService userService, IAccountService accountService, ICoinService coinService, IPriceService priceService, IHistoryService historyService) {
+        this.userService = userService;
+        this.accountService = accountService;
+        this.coinService = coinService;
+        this.priceService = priceService;
+        this.historyService = historyService;
+    }
+
+    private void validateUser(String userId) {
+        if (userService.getUser(userId) == null) {
+            throw new UserNotFoundException("The user data doesn't exist!");
+        }
+    }
+
+    private Coin validateCoin(Integer coinId) {
+        Coin coin = coinService.getCoin(coinId);
+        if (coin == null) {
+            throw new ServiceException("The coin data doesn't exist!");
+        }
+        return coin;
+    }
+
+    private void validateAccount(String userId, Integer coinId, Coin coin) {
+        if (accountService.getAccount(userId, coinId) == null) {
+            throw new ServiceException("The user doesn't have a(n) " + coin.getCoinName() + " account!");
+        }
+    }
+
+    private void updateHistory(History history) {
+        int currentPrice = priceService.getPrice();
+        history.setTransPrice(BigDecimal.valueOf(currentPrice));
+
+        updateBalance(history);
+        updateUSD(history);
+
+        history.setTransTime(Date.from(Instant.now()));
+    }
+
+    private void updateBalance(History history) {
+        Account accountQuery = accountService.getAccount(history.getUserId(), history.getCoinId());
+        history.setBeforeBalance(accountQuery.getNetValue());
+        BigDecimal changeAmount = getChangeAmount(history, history.getTransQuantity());
+        history.setAfterBalance(history.getBeforeBalance().add(changeAmount));
+    }
+
+    private void updateUSD(History history) {
+        Coin coinUSD = coinService.getCoin("USD");
+        Account accountUSD = accountService.getAccount(history.getUserId(), coinUSD.getCoinId());
+        history.setBeforeBalanceUSD(accountUSD.getNetValue());
+        BigDecimal changeAmountUSD = getChangeAmountUSD(history, history.getTransQuantity());
+        history.setAfterBalanceUSD(history.getBeforeBalanceUSD().add(changeAmountUSD));
+    }
+
+    private BigDecimal getChangeAmount(History history, BigDecimal quantity) {
+        return history.getTransType().equals(String.valueOf(TradeDirection.BUY))
+                ? quantity
+                : quantity.multiply(BigDecimal.valueOf(-1));
+    }
+
+    private BigDecimal getChangeAmountUSD(History history, BigDecimal quantity) {
+        int price = priceService.getPrice();
+        return history.getTransType().equals(String.valueOf(TradeDirection.BUY))
+                ? quantity.multiply(BigDecimal.valueOf(-1 * price))
+                : quantity.multiply(BigDecimal.valueOf(price));
+    }
 
     @PostMapping("/save")
     public Integer createTrade(@RequestBody History history) {
-        User userQuery = userService.getUser(history.getUserId());
-        if (userQuery == null) {
-            throw new UserNotFoundException("The user data doesn't exist!");
-        }
-        Coin coinQuery = coinService.getCoin(history.getCoinId());
-        if (coinQuery == null) {
-            throw new ServiceException("The coin data doesn't exist!");
-        }
-        Coin coinUSD = coinService.getCoin("USD");
-        if (coinUSD == null) {
-            throw new ServiceException("The coin data doesn't exist!");
-        }
-        Account accountQuery = accountService.getAccount(history.getUserId(), history.getCoinId());
-        if (accountQuery == null) {
-            throw new ServiceException("The user doesn't have a(n) " + coinQuery.getCoinName() + " account!");
-        }
-        Account accountUSD = accountService.getAccount(history.getUserId(), coinUSD.getCoinId());
-        if (accountUSD == null) {
-            throw new UserNotFoundException("The user doesn't have an USD account!");
-        }
-        Instant instant = Instant.now();
-        int price = priceService.getPrice();
-        history.setTransPrice(BigDecimal.valueOf(price));
-        BigDecimal changeAmount;
-        if (history.getTransType().equals(String.valueOf(TradeDirection.BUY))) {
-            changeAmount = history.getTransQuantity();
-        } else {
-            changeAmount = history.getTransQuantity().multiply(BigDecimal.valueOf(-1));
-        }
-        history.setBeforeBalance(accountQuery.getNetValue());
-        history.setAfterBalance(history.getBeforeBalance().add(changeAmount));
-        if (history.getTransType().equals(String.valueOf(TradeDirection.BUY))) {
-            changeAmount = history.getTransQuantity().multiply(BigDecimal.valueOf(-1 * price));
-        } else {
-            changeAmount = history.getTransQuantity().multiply(BigDecimal.valueOf(price));
-        }
-        history.setBeforeBalanceUSD(accountUSD.getNetValue());
-        history.setAfterBalanceUSD(history.getBeforeBalanceUSD().add(changeAmount));
-        history.setTransTime(Date.from(instant));
+        validateUser(history.getUserId());
+        Coin coin = validateCoin(history.getCoinId());
+        validateAccount(history.getUserId(), history.getCoinId(), coin);
+        updateHistory(history);
         historyService.createHistory(history);
         return 1;
     }
